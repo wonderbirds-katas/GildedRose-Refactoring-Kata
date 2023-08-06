ab3e2a09150e6ea0a48249762371dd902870704a
Stefan Boos <kontakt@boos.systems>
2023-08-06 18:43:01 +0200

    test: standard item sellin valueˆ

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -7,15 +7,17 @@ namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
-    [Fact]
-    public void UpdateStandardItem()
+    [Theory]
+    [InlineData("foo", 0, 0, -1, 0)]
+    [InlineData("foo", 10, 0, 9, 0)]
+    public void UpdateStandardItem(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
     {
-        var input = new Item {Name = "foo", SellIn = 0, Quality = 0};
+        var input = new Item {Name = name, SellIn = sellIn, Quality = quality};
         var items = new List<Item> {input};
 
         new GildedRose.GildedRose(items).UpdateQuality();
         
-        var expected = new Item {Name = "foo", SellIn = -1, Quality = 0};
+        var expected = new Item {Name = "foo", SellIn = expectedSellIn, Quality = expectedQuality};
         items[0].Should().BeEquivalentTo(expected);
     }
 }
