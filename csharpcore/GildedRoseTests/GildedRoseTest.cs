b52de46c6fdcf72a5cff1a5eead6030abc6075a5
Stefan Boos <kontakt@boos.systems>
2023-08-06 19:18:47 +0200

    test: override test descriptionˆ

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -7,17 +7,32 @@ namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
-    [Theory]
-    [InlineData("foo", 0, 0, -1, 0)]
-    [InlineData("foo", 10, 0, 9, 0)]
-    public void UpdateStandardItem(string name, int sellIn, int quality, int expectedSellIn, int expectedQuality)
+    public class TestItem {
+        public Item Item { get; set; }
+        public string Description { get; set; }
+        public override string ToString() => Description;
+    }
+    
+    public static TheoryData<TestItem> ItemsForWhichSellInShallDecreaseByOne()
     {
-        var input = new Item {Name = name, SellIn = sellIn, Quality = quality};
-        var items = new List<Item> {input};
-
+        var data = new TheoryData<TestItem>();
+        data.Add(new TestItem
+        {
+            Item = new Item{Name = "foo", SellIn = -5, Quality = 0},
+            Description = "negative sellin"
+        });
+        return data;
+    }
+    
+    [Theory(DisplayName = "For standard item, reduce sellin by 1")]
+    [MemberData(nameof(ItemsForWhichSellInShallDecreaseByOne))]
+    public void GivenStandardItem_WhenUpdateQuality_ThenSellInDecreasesByOne(TestItem item)
+    {
+        var expectedSellIn = item.Item.SellIn - 1;
+        
+        var items = new List<Item> {item.Item};
         new GildedRose.GildedRose(items).UpdateQuality();
         
-        var expected = new Item {Name = "foo", SellIn = expectedSellIn, Quality = expectedQuality};
-        items[0].Should().BeEquivalentTo(expected);
+        items[0].SellIn.Should().Be(expectedSellIn);
     }
 }
