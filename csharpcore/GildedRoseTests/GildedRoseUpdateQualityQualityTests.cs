1fcb5e5e9d9da82a9216520154fff801ec42d93d
Stefan Boos <kontakt@boos.systems>
2023-08-07 11:21:33 +0200

    test: reduce quality for standard items

---
+++ csharpcore/GildedRoseTests/GildedRoseUpdateQualityQualityTests.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using FluentAssertions;
+using GildedRose;
+
+namespace GildedRoseTests;
+
+public class GildedRoseUpdateQualityQualityTests
+{
+    [Theory(DisplayName = "For standard items, reduce quality by 1 ")]
+    [MemberData(nameof(ItemsForWhichQualityShallDecreaseByOne))]
+    public void GivenStandardItem_WhenUpdateQuality_ThenQualityDecreasesByOne(TestItem item)
+    {
+        var expectedQuality = item.Item.Quality - 1;
+
+        var items = new List<Item> {item.Item};
+        new GildedRose.GildedRose(items).UpdateQuality();
+
+        items[0].Quality.Should().Be(expectedQuality);
+    }
+
+    public static TheoryData<TestItem> ItemsForWhichQualityShallDecreaseByOne() =>
+        new()
+        {
+            new TestItem("foo", -5, 1),
+            new TestItem("foo", -1, 1),
+            new TestItem("foo", 0, 1),
+            new TestItem("foo", 1, 1),
+            new TestItem("foo", 5, 1),
+        };
+
+    public class TestItem
+    {
+        public Item Item { get; }
+
+        public TestItem(string name, int sellIn, int quality)
+        {
+            Item = new Item { Name = name, SellIn = sellIn, Quality = quality };
+        }
+
+        public override string ToString() => $"quality: {Item.Quality}, sellIn: {Item.SellIn}";
+    }
+}
