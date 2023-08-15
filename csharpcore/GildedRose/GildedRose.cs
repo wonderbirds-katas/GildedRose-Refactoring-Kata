baed4dcf3d7296ee48f76fdacaa64536e0f5afa4
Stefan Boos <kontakt@boos.systems>
2023-08-15 05:35:38 +0200

    refactor: merge if statements

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -57,20 +57,14 @@ public class GildedRose
             item.Quality -= item.Quality;
         }
 
-        if (item.Name != "Aged Brie" && item.SellIn < 0)
+        if (item.Name != "Aged Brie" && item.SellIn < 0 && item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
         {
-            if (item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
-            {
-                item.Quality -= 1;
-            }
+            item.Quality -= 1;
         }
         
-        if (item.Name == "Aged Brie" && item.SellIn < 0)
+        if (item.Name == "Aged Brie" && item.SellIn < 0 && item.Quality < 50)
         {
-            if (item.Quality < 50)
-            {
-                item.Quality += 1;
-            }
+            item.Quality += 1;
         }
     }
 
