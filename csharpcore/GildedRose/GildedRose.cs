6dd214fd4454f6ec026c236b769c7877571bd8c2
Stefan Boos <kontakt@boos.systems>
2023-08-15 05:19:34 +0200

    refactor: break up if .. else

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -33,7 +33,14 @@ public class GildedRose
                 item.Quality -= 1;
             }
         }
-        else
+        if (item.Name == "Aged Brie")
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality += 1;
+            }
+        }
+        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
         {
             if (item.Quality < 50)
             {
